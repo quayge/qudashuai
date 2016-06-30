455b480d428da4e291fdece1502ce539c1ce7a2f
yinge <1294857268@qq.com>
2016-06-30 17:38:41 +0800

    merge yy

--- app/routes/users.js
+++ app/routes/users.js
@@ -1,9 +1,12 @@
 var express = require('express');
-var router = express.Router();
+var  ro= express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
+ro.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
-module.exports = router;
+ro.get("/first",function(req,res,next){
+	var aks="fjf"
+})
+module.exports = ro;
